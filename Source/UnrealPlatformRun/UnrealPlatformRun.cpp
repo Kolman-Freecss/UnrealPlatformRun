1aafebd2bdf433574dccb3a4da710c0c55020660
Kolman-Freecss <s3rg10.martinez.roman@gmail.com>
2023-04-18 13:13:45 +0200

    First commit

---
+++ Source/UnrealPlatformRun/UnrealPlatformRun.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "UnrealPlatformRun.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, UnrealPlatformRun, "UnrealPlatformRun" );
---
+++ Source/UnrealPlatformRun/UnrealPlatformRunGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "UnrealPlatformRunGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class UNREALPLATFORMRUN_API AUnrealPlatformRunGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
